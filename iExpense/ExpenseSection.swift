a4d7a6fee698df7053b88e98f2200d06eda65a01
Güray Gül <guraygul59@gmail.com>
2023-11-24 23:48:03 +0300

    Expenses list splitted into two sections

---
+++ iExpense/ExpenseSection.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseSection.swift
+//  iExpense
+//
+//  Created by Güray Gül on 24.11.2023.
+//
+
+import SwiftUI
+
+struct ExpenseSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseSection()
+}
---
+++ iExpense/View-ExpenseStyle.swift
@@ -0,0 +1,8 @@
+//
+//  View-ExpenseStyle.swift
+//  iExpense
+//
+//  Created by Güray Gül on 24.11.2023.
+//
+
+import Foundation
